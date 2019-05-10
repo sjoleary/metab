60df9c700b4337f19d6ddf7577455814e7fb031a
alexiscarter <alexis.carteron@gmail.com>
2019-05-10 13:37:06 -0400

    script for ITS to be tested

--- dada_script_ITS.R
+++ dada_script_ITS.R
@@ -6,8 +6,8 @@ library(Biostrings); packageVersion("Biostrings")
 path <- "data/ITS_sub"
 list.files(path)
 
-fnFs <- sort(list.files(path, pattern = "_R1.fastq.gz", full.names = TRUE)) # /!\ It seems that this workflow with cutadapt need gzip files...
-fnRs <- sort(list.files(path, pattern = "_R2.fastq.gz", full.names = TRUE))
+fnFs <- sort(list.files(path, pattern = "_R1.fastq", full.names = TRUE)) # /!\ It seems that this workflow with cutadapt need gzip files...
+fnRs <- sort(list.files(path, pattern = "_R2.fastq", full.names = TRUE))
 
 FWD <- "GATGAAGAACGYAGYRAA"
 REV <- "TCCTCCGCTTATTGATATGC"
@@ -31,7 +31,7 @@ sample.names <- sapply(strsplit(fnFs, "_"), `[`, 1)
 
 fnFs.filtN <- file.path(path, "filtN", basename(fnFs)) # Put N-filterd files in filtN/ subdirectory
 fnRs.filtN <- file.path(path, "filtN", basename(fnRs))
-out <- filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, multithread = TRUE)
+out <- filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, multithread = TRUE, compress = FALSE)
 
 primerHits <- function(primer, fn) {
   # Counts number of reads in which the primer is found
@@ -43,7 +43,7 @@ rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.filtN[[1]]),
       REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.filtN[[1]]), 
       REV.ReverseReads = sapply(REV.orients, primerHits, fn = fnRs.filtN[[1]]))
 
-cutadapt <- "/Users/carteronalexis/.local/bin/cutadapt" # CHANGE ME to the cutadapt path on your machine
+cutadapt <- "/home/udem/.local/bin/cutadapt" # CHANGE ME to the cutadapt path on your machine
 system2(cutadapt, args = "--version") # Run shell commands from R
 
 path.cut <- file.path(path, "cutadapt")
@@ -70,14 +70,15 @@ rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.cut[[1]]),
       REV.ReverseReads = sapply(REV.orients, primerHits, fn = fnRs.cut[[1]]))
 
 # Forward and reverse fastq filenames have the format:
-cutFs <- sort(list.files(path.cut, pattern = "_R1.fastq.gz", full.names = TRUE))
-cutRs <- sort(list.files(path.cut, pattern = "_R2.fastq.gz", full.names = TRUE))
+cutFs <- sort(list.files(path.cut, pattern = "_R1.fastq", full.names = TRUE))
+cutRs <- sort(list.files(path.cut, pattern = "_R2.fastq", full.names = TRUE))
 
 # Extract sample names, assuming filenames have format:
 get.sample.name <- function(fname) strsplit(basename(fname), "_")[[1]][1]
 sample.names <- unname(sapply(cutFs, get.sample.name))
 head(sample.names)
 
+plotQualityProfile(fnFs[1:2])
 plotQualityProfile(cutFs[1:2])
 
 filtFs <- file.path(path.cut, "filtered", basename(cutFs))
@@ -133,6 +134,9 @@ hist(nchar(getSequences(seqtab.nochim)),xlab="Size", ylab="Frequency", main = "N
 #                          refFasta = "reference_database/sh_general_release_dynamic_01.12.2017.fasta",
 #                          minBoot = 50, #Default 50. The minimum bootstrap #confidence for # assigning a taxonomic level.
 #                          multithread=TRUE)
+
+
+
 
 corr <- as.data.frame(cor(seqtab.nochim, method = "spearman"))
 indices <- as.data.frame(which(corr > .8 & corr <1, arr.ind = TRUE))
